760a9131197c26bbf843fe8ae98d7c671a50d6ff
sandeepnegi1996 <9041128179sandy@gmail.com>
2021-08-19 23:37:20 +0530

    basic ui done

---
+++ src/AudioPlayer/AudioControls/AudioControls.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+import { PlayFill } from 'react-bootstrap-icons'
+import { PauseFill } from 'react-bootstrap-icons'
+import { ArrowLeftCircleFill } from 'react-bootstrap-icons'
+import { ArrowRightCircleFill } from 'react-bootstrap-icons'
+import './AudioControl.css'
+
+const AudioControls = ({
+  isPlaying,
+  onPlayPauseClick,
+  onPrevClick,
+  onNextClick,
+}) => {
+  return (
+    <div className='audio-controls'>
+      <button
+        type='button'
+        className='prev'
+        aria-label='Previous'
+        onClick={onPrevClick}
+      >
+        <ArrowLeftCircleFill size={40} color={'#90EE90'} />
+      </button>
+
+      {isPlaying ? (
+        <button
+          type='button'
+          className='pause'
+          aria-label='Pause'
+          onClick={() => {
+            onPlayPauseClick(false)
+          }}
+        >
+          <PauseFill size={40} color={'#90EE90'} />
+        </button>
+      ) : (
+        <button
+          type='button'
+          className='play'
+          onClick={() => onPlayPauseClick(true)}
+          aria-label='Play'
+        >
+          <PlayFill size={40} color={'#90EE90'} />
+        </button>
+      )}
+
+      <button
+        type='button'
+        className='next'
+        aria-label='Next'
+        onClick={onNextClick}
+      >
+        <ArrowRightCircleFill size={40} color={'#90EE90'} />
+      </button>
+    </div>
+  )
+}
+
+export default AudioControls
